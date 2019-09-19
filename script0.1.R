10fbd3473ce7af8df0845b1ee39ab00cb369d368
Rick Weber <rick.weber05@gmail.com>
2019-09-19 10:52:47 -0400

    Adding basic scripts

---
+++ script0.1.R
@@ -0,0 +1,7 @@
+# Here's what I'm doing...
+# Taking data from the County Business Patterns dataset
+# I'm going to build an index of economic complexity at the county level.
+# I'll use that to make maps and run regressions on economic growth
+# as well as other interesting variables.
+# But first, let's set up the project:
+source("setup.R")
---
+++ setup.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(tidycensus)
+
+# import custom functions
+source("functions.R")
+
+# import data
+source("import_data.R")
